53e8a5a2cdc244a0ed0921b4e51584716ea701f7
mygithub253 <82616422+mygithub253@users.noreply.github.com>
2021-05-28 18:42:56 +0530

    Update listreport.java

--- Dtrack v_almostFinal/app/src/main/java/com/example/finalapp/listreport.java
+++ Dtrack v_almostFinal/app/src/main/java/com/example/finalapp/listreport.java
@@ -30,7 +30,7 @@ public class listreport extends AppCompatActivity {
     FirebaseDatabase database;
     DatabaseReference ref;
     ArrayList<String> list;
-    ArrayAdapter<String> adapter;
+    ArrayAdapter<String> adapter;                       //initialising variables
     Bio bio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +41,8 @@ public class listreport extends AppCompatActivity {
         Intent intent=getIntent();
         String key=intent.getExtras().getString("unq");
         ref = database.getReference("Users").child(key).child("data");
-        list = new ArrayList<>();
-        bio = new Bio();
+        list = new ArrayList<>();                                               //creating empty arraylist
+        bio = new Bio();                                    //using bio.java class to get glucose, bp , weight and date stamp from firebase
 
         adapter = new ArrayAdapter<String>(this, R.layout.user_info, R.id.userInfo, list);
 
@@ -51,7 +51,7 @@ public class listreport extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 int i=1;
                 for (DataSnapshot ds : snapshot.getChildren()) {
-                    bio = ds.getValue(Bio.class);
+                    bio = ds.getValue(Bio.class);                                   //converting data from firebase to string and diplaying it in listview
 
                     list.add(String.valueOf(i)+") " +  bio.getDate().toString() + "\n     Blood Glucose : " + bio.getBg().toString() + "\n     Blood Pressure : " + bio.getBp().toString() + "\n     Weight : " + bio.getWeight().toString());
                     //list.add(String.valueOf(i)+") Blood Glucose : " + bio.getBg().toString() + "\n     Blood Pressure : " + bio.getBp().toString() + "\n     Weight : " + bio.getWeight().toString());
